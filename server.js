e3ada391e75463be1a0ee0ab2a4794bd5bdb9363
d1son <dson120@gmail.com>
2016-02-10 19:16:42 -0500

    finished game of thrones lineage classwork, not working right now

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout: 'gameofthrones'}));
+app.set('view engine', 'handlebars');
+
+var lineage = {
+    lannister: {
+        house: 'House Lannister',
+        parents: {
+            first_parents: 'Joanna & Tywin',
+            second_parents: 'Devan & Dorna'
+        },
+        child: {
+            first_child: 'Cersei',
+            second_child: 'Jaime',
+            third_child: 'Tyrion',
+            fourth_child: 'Lancel'
+        }
+    },
+    targaryen: {
+        house: 'House Targaryen',
+        parents: 'Aerys II & Rhaella',
+        child: {
+            first_child: 'Rhaegar',
+            second_child: 'Viserys',
+            third_child: 'Daenerys'
+        }
+    },
+    stark: {
+        house: 'House Stark',
+        parents: 'Eddard & Catelyn',
+        child: {
+            first_child: 'Robb',
+            second_child: 'Sansa',
+            third_child: 'Arya',
+            fourth_child: 'Bran',
+            fifth_child: 'Rickon'       
+        }
+    },
+};
+
+app.get("/game/of/thrones", function(req, res) {
+	res.sendFile("thrones", lineage)
+})
+
+app.listen(PORT, function() {
+  console.log('Server listening on %s', PORT);
+});
